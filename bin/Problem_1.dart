25b347df66d2c98df56151550e8c60358cb61334
Sadmanzahid <zahid15-5472@diu.edu.bd>
2025-09-15 22:26:32 +0600

    Init Commit

---
+++ bin/Problem_1.dart
@@ -0,0 +1,16 @@
+class Student{
+  String ? Name;
+  int ? Roll;
+
+  Student(this.Name,this.Roll);
+  displayInfo(){
+    print("Name: $Name");
+    print("Roll No: $Roll");
+  }
+
+}
+main(){
+  Student s =Student("Sadman", 1);
+  s.displayInfo();
+
+}
