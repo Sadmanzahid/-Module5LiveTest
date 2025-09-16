905b2576933e92564365ce4cacc7468a7f7019e6
Sadmanzahid <zahid15-5472@diu.edu.bd>
2025-09-16 12:43:38 +0600

    Init Commit

--- bin/Problem_3.dart
+++ bin/Problem_3.dart
@@ -1,18 +1,33 @@
 abstract class Shape {
+  double? _area;
+
   void area();
+
+  void setArea(double value) {
+    _area = value;
+  }
+
+
+  double getArea() {
+    return _area ?? 0.0;
+  }
 }
+
+
 class Circle extends Shape {
   double radius;
-  double? areaValue;
+
   Circle(this.radius);
 
   @override
   void area() {
-    areaValue = 3.1416 * radius * radius;
-    print("Circle Area: $areaValue");
+    double result = 3.1416 * radius * radius;
+    setArea(result);  // store area using setter
   }
 }
+
 void main() {
-  Circle c1 = Circle(15);
-  c1.area();
+  Circle c1 = Circle(13);
+  c1.area(); // calculate and set area
+  print("Circle Area: ${c1.getArea()}");
 }
